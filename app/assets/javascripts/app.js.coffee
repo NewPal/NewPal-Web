993986464174bb8f99d544447665c6545eeb716a
honor <katalina@mustatea.com>
2014-02-08 23:16:55 -0500

    angular 2

---
+++ app/assets/javascripts/app.js.coffee
@@ -0,0 +1,1 @@
+window.App = angular.module('NewpalWeb', ['ngResource'])
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,7 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
+//= require app
 //= require angular
 //= require turbolinks
 //= require_tree .

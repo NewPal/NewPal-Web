a4bd122af2859dd3ccf49ebdd165335715e6d6c5
honor <katalina@mustatea.com>
2014-02-08 19:38:39 -0500

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 NewpalWeb::Application.routes.draw do
-  get "statis_pages/home"
+  root  'statis_pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
